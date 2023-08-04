3f0c987c8be376e83a450e0bae61d622f5faf276
matiasezequielsilva <matiasezequielsilva@gmail.com>
2023-08-03 22:41:21 -0300

    python add

---
+++ python/RecorrerDiccionario.py
@@ -0,0 +1,9 @@
+#recorriendo todos los key-value pairs
+fav_numbers = {'eric' : 17, 'ever' : 4}
+for name, number in fav_numbers.items():
+    print(name + ' loves ' + str(number))
+# todos los keys
+for name in fav_numbers.keys():
+    print(name + ' loves a number')
+for number in fav_numbers.values():
+    print(str(number) + ' is a favorite number for someone')
