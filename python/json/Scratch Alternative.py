dc46f9bd31c84fe192fe6b4d9fb6feff63dcc6da
matiasezequielsilva <matiasezequielsilva@gmail.com>
2023-09-13 03:31:39 -0300

    mejorando Json Alternative

--- python/json/Scratch Alternative.py
+++ python/json/Scratch Alternative.py
@@ -1,3 +1,4 @@
+#Este archivo crea un diccionario filtered_data para hacer una lista solo de filtered todos y obtener solo un objeto json
 import json
 import requests
 import os
@@ -6,7 +7,6 @@ script_dir = os.path.dirname(os.path.realpath(__file__))
 os.chdir(script_dir)
 
 response = requests.get("https://jsonplaceholder.typicode.com/todos")
-#todos = json.loads(response.text)
 todos = response.json()
 # Map of userId to number of complete TODOs for that user
 todos_by_user = {}
@@ -37,14 +37,20 @@ for user, num_complete in top_users:
     users.append(str(user))
 
 max_users = " and ".join(users)
-# Define a function to filter out completed TODOs 
-# of users with max completed TODOS.
+
+# Define a dictionary to hold the filtered data.
+filtered_data = {
+    "filtered_todos": []
+}
+
+# Populate the filtered_todos list.
 def keep(todo):
     is_complete = todo["completed"]
     has_max_count = str(todo["userId"]) in users
     return is_complete and has_max_count
 
-# Write filtered TODOs to file.
-with open("filtered_data_file.json", "w") as data_file:
-    filtered_todos = list(filter(keep, todos))
-    json.dump(filtered_todos, data_file, indent=2)
+filtered_data["filtered_todos"] = list(filter(keep, todos))
+
+# Write filtered data to file.
+with open("filtered_data_file_Alternative.json", "w") as data_file:
+    json.dump(filtered_data, data_file, indent=2)
